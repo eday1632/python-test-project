7a203400943260af4093560e9c6c866ecb59f99b
Eric Day <ericday@Erics-MacBook-Air.local>
2019-10-15 13:59:58 -0700

    implemented funcs, tests

--- src/my_module.py
+++ src/my_module.py
@@ -1,2 +1,71 @@
-def addOne(number):
-    return number + 1
+
+
+class Rover(object):
+
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.bearing = None
+        self.width_limit = None
+        self.height_limit = None
+
+    def land_rover(self, init, limits):
+        # sets the initial position and bearing
+        init = init.split()
+        limits = limits.split()
+
+        self.x = int(init[0])
+        self.y = int(init[1])
+        self.bearing = init[2]
+        self.width_limit = int(limits[0])
+        self.height_limit = int(limits[1])
+
+    def move(self):
+        # todo finish testing paths
+        if self.bearing == 'N':
+            if self.y < self.height_limit:
+                self.y += 1
+        elif self.bearing == 'E':
+            if self.x < self.width_limit:
+                self.x += 1
+        elif self.bearing == 'S':
+            if self.y > 0:
+                self.y -= 1
+        elif self.bearing == 'W':
+            if self.x > 0:
+                self.x -= 1
+
+    def left(self):
+        if self.bearing == 'N':
+            self.bearing = 'W'
+        elif self.bearing == 'E':
+            self.bearing = 'N'
+        elif self.bearing == 'S':
+            self.bearing = 'E'
+        elif self.bearing == 'W':
+            self.bearing = 'S'
+
+    def right(self):
+        if self.bearing == 'N':
+            self.bearing = 'E'
+        elif self.bearing == 'E':
+            self.bearing = 'S'
+        elif self.bearing == 'S':
+            self.bearing = 'W'
+        elif self.bearing == 'W':
+            self.bearing = 'N'
+
+    def current_location(self):
+        return self.x, self.y, self.bearing
+
+    def conduct_mission(self, moves):
+        for move in moves:
+            if move == 'L':
+                self.left()
+            elif move == 'R':
+                self.right()
+            elif move == 'M':
+                self.move()
+
+
+
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,6 +3,70 @@ from context import src
 from src import my_module
 
 class MyTest(TestCase):
-    def test(self):
-        result = my_module.addOne(1)
-        self.assertEqual(result, 3)
+    # todo: setup initial test variables / objects
+
+    def test_current_location(self):
+        rover = my_module.Rover()
+        rover.land_rover('1 2 N', '5 5')
+        self.assertEqual(rover.current_location(), (1, 2, 'N'))
+
+    def test_move(self):
+        rover = my_module.Rover()
+        rover.land_rover('1 2 N', '5 5')
+
+        rover.move()
+        self.assertEqual(rover.current_location(), (1, 3, 'N'))
+
+        rover.move()
+        rover.move()
+        self.assertEqual(rover.current_location(), (1, 5, 'N'))
+
+        rover.move()
+        self.assertEqual(rover.current_location(), (1, 5, 'N'))
+
+    def test_left(self):
+        rover = my_module.Rover()
+        rover.land_rover('1 2 N', '5 5')
+
+        rover.left()
+        self.assertEqual(rover.current_location(), (1, 2, 'W'))
+
+        rover.left()
+        self.assertEqual(rover.current_location(), (1, 2, 'S'))
+
+        rover.left()
+        self.assertEqual(rover.current_location(), (1, 2, 'E'))
+
+        rover.left()
+        self.assertEqual(rover.current_location(), (1, 2, 'N'))
+
+    def test_right(self):
+        rover = my_module.Rover()
+        rover.land_rover('1 2 N', '5 5')
+
+        rover.right()
+        self.assertEqual(rover.current_location(), (1, 2, 'E'))
+
+        rover.right()
+        self.assertEqual(rover.current_location(), (1, 2, 'S'))
+
+        rover.right()
+        self.assertEqual(rover.current_location(), (1, 2, 'W'))
+
+        rover.right()
+        self.assertEqual(rover.current_location(), (1, 2, 'N'))
+
+    def test_conduct_mission(self):
+        rover = my_module.Rover()
+        rover.land_rover('1 2 N', '5 5')
+
+        rover.conduct_mission('LMLMLMLMM')
+        self.assertEqual(rover.current_location(), (1, 3, 'N'))
+
+        rover = my_module.Rover()
+        rover.land_rover('3 3 E', '5 5')
+
+        rover.conduct_mission('MMRMMRMRRM')
+        self.assertEqual(rover.current_location(), (5, 1, 'E'))
+
+
